c3770b59492819ed2702bdf175e3a5be82041c32
naveetha2003 <navee222003@gmail.com>
2026-06-13 10:54:36 +0100

    updated files

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -16,7 +16,8 @@ function Product({ wishlist, setWishlist, cart, setCart,search }) {
 
   //  FETCH PRODUCTS
   useEffect(() => {
-    fetch(`${API}/api/products`)
+    fetch("https://techzone-backend-3.onrender.com/api/products")
+    
       .then((res) => res.json())
       .then((data) => setProducts(data))
       .catch((err) => console.log(err))
